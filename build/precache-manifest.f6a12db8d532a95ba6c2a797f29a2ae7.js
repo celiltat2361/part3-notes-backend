c67cec84b882200a24b74f0d84df4b011bc04a27
celil.tat <celilbtc@gmail.com>
2020-07-21 20:45:57 +0200

    Added frontend

---
+++ build/precache-manifest.f6a12db8d532a95ba6c2a797f29a2ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b135268873d66170f357ff089a3d118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dbd5e7b99cf82dbd3f0",
+    "url": "/static/css/main.4187a5f6.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7dbd5e7b99cf82dbd3f0",
+    "url": "/static/js/main.4e3d9316.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
